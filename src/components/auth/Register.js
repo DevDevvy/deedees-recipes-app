915ebc13a4dfcc40b3458ba75b2885cd6f31259d
Randall Thomas <randallthomasmusic@gmail.com>
2022-03-18 14:13:13 -0700

    found register problem, api call wasnt changed

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -9,7 +9,7 @@ export const Register = (props) => {
     const history = useHistory()
 
     const existingUserCheck = () => {
-        return fetch(`http://localhost:8088/users?email=${user.email}`)
+        return fetch(`https://deedees-api-qdte8.ondigitalocean.app/users?email=${user.email}`)
             .then(res => res.json())
             .then(user => !!user.length)
     }
@@ -18,7 +18,7 @@ export const Register = (props) => {
         existingUserCheck()
             .then((userExists) => {
                 if (!userExists) {
-                    fetch("http://localhost:8088/users", {
+                    fetch("https://deedees-api-qdte8.ondigitalocean.app/users", {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/json"
